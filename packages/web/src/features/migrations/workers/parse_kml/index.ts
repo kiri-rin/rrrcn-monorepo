01b11203474ddee4eb270ced57a0c77799ab11ee
kiri <kirillknizhov@mail.ru>
2023-12-11 23:13:46 +0700

    cross validation random points WIP

--- packages/web/src/features/migrations/workers/parse_kml/index.ts
+++ packages/web/src/features/migrations/workers/parse_kml/index.ts
@@ -1,49 +1,55 @@
 import "@react-google-maps/api";
-import { ParseTrackerXML, WorkerMessage } from "./types";
+import { ParseTrackerXML, TrackerFileTypes, WorkerMessage } from "./types";
 import { parseAquilaKML } from "./aquila";
 import { Migration } from "../../types";
-
 const DOMParser = new (require("xmldom").DOMParser)();
 const queue: WorkerMessage[] = [];
 let processing = false;
+/* eslint-disable-next-line no-restricted-globals */
+const processData = (
+  data: WorkerMessage | undefined,
+  callback: (res: { result: any; id: any }) => void
+) => {
+  if (!data) {
+    return;
+  }
+  const { type, file, id } = data;
+
+  let parser: ParseTrackerXML;
+  switch (type) {
+    case TrackerFileTypes.ORNITELLA:
+    case TrackerFileTypes.AQUILA:
+    default:
+      parser = parseAquilaKML;
+      break;
+  }
+  console.log("START PROCESSING", data);
+
+  parser(file, DOMParser)
+    .then((res) => {
+      console.log("finished", id);
+      callback({ result: res, id });
+    })
+    .catch((err) => {
+      console.log("finished with error", id);
+      console.log(err);
+      processing = false;
+
+      callback({ result: null, id });
+    })
+    .finally(() => {
+      processData(queue.pop(), callback);
+    });
+};
 /* eslint-disable-next-line no-restricted-globals */
 self.onmessage = async (e: MessageEvent<WorkerMessage>) => {
   if (!e.data) {
     return;
   }
-  const { type, file, id } = e.data;
-  const processData = (data: any) => {
-    let parser: ParseTrackerXML;
-    switch (e.data.type) {
-      case "aquila":
-      default:
-        parser = parseAquilaKML;
-        break;
-    }
-    console.log("START PROCESSING", data);
 
-    if (!data) {
-      return;
-    }
-    parser(file, DOMParser)
-      .then((res) => {
-        console.log("finished", id);
-        /* eslint-disable-next-line no-restricted-globals */
-        self.postMessage({ result: res, id });
-      })
-      .catch((err) => {
-        console.log("finished with error", id);
-        console.log(err);
-        processing = false;
-
-        /* eslint-disable-next-line no-restricted-globals */
-        self.postMessage({ result: null, id });
-      })
-      .finally(() => {
-        processData(queue.pop());
-      });
-  };
-
-  queue.length || processing ? queue.unshift(e.data) : processData(e.data);
+  queue.length || processing
+    ? queue.unshift(e.data)
+    : /* eslint-disable-next-line no-restricted-globals */
+      processData(e.data, self.postMessage);
 };
 export {};
---
+++ packages/web/src/features/migrations/workers/parse_kml/ornitella.ts
@@ -0,0 +1,64 @@
+import type { ParseTrackerXML } from "./types";
+import { kml } from "@tmcw/togeojson";
+import { Feature, GeoJSON } from "geojson";
+import { parse } from "date-fns";
+import { parseHTMLTable } from "./utils";
+import { Migration, MigrationPointProperties } from "../../types";
+import { SetTimeoutPromise } from "../../utils";
+const DATE_PROP = "timestamp";
+const DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
+
+export const parseOrnitellaKML: ParseTrackerXML = async (file, DOMParser) => {
+  const text = await file.text();
+  const geojson = kml(
+    DOMParser.parseFromString(text, "text/xml")
+  ) as GeoJSON.FeatureCollection;
+  let features: Migration["geojson"]["features"] = [];
+  let index = 0;
+  for (let it of geojson.features) {
+    index++;
+    if (index % 100 === 0) {
+      await SetTimeoutPromise(0);
+    }
+    if (it.geometry.type === "Point") {
+      features.push({
+        ...it,
+        properties: {
+          ...it.properties,
+          elevation: parseInt(
+            (
+              parseHTMLTable(
+                DOMParser.parseFromString(
+                  it.properties?.description?.value || "<div></div>",
+                  "text/html"
+                )
+              ).find((it) => it?.[0]?.includes("Altitude"))?.[1] || "0"
+            ).replace(/\D/g, "")
+          ),
+          date: parse(
+            it.properties?.[DATE_PROP] || "",
+            DATE_FORMAT,
+            new Date()
+          ),
+        },
+      } as Feature<GeoJSON.Point, MigrationPointProperties>);
+    }
+  }
+  features = [...features].sort((a, b) =>
+    a.properties?.date > b.properties?.date ? 1 : -1
+  );
+  features = features.map((it, index) => ({
+    ...it,
+    properties: { ...it.properties, index },
+  }));
+  const res: Migration["geojson"] = {
+    features,
+    type: geojson.type,
+    bbox: geojson.bbox,
+  };
+  return {
+    geojson: res,
+    years: {} as Migration["years"],
+    title: file.name,
+  };
+};
